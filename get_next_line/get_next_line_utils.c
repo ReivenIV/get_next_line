9ed7f81ceaf5ac936ade7b0ed2bc5c624edd6288
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-10 14:03:19 +0100

    substr created

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,14 +6,14 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:12:47 by rita              #+#    #+#             */
-/*   Updated: 2024/12/10 12:45:19 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/10 14:03:03 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // TODO strlen		DONE
 // TODO strchr		DONE
-// TODO strjoin
-// TODO substr
+// TODO strjoin		DONE
+// TODO substr		DONE
 
 #include "get_next_line_bonus.h"
 
@@ -79,3 +79,31 @@ char	*ft_strjoin(char const *src1, char const *src2)
 	return (str);		
 }
 
+// will output from a "src", "star", "len" one string
+// ex: "Hello, World!", 7, 5 = > "World"
+// ex: "Hello, World!", 7, 50 = > "World!"
+// ex: "Hello, World!", 70, 5 = > ""
+// R : ""if (start >= src_len) return (ft_strdup(""));"" is not in the function 
+char	*ft_substr(char const *src, int start, int len)
+{
+	int	i;
+	int	src_len;
+	char	*str;
+
+	if (src == NULL)
+		return (NULL);
+	src_len = ft_strlen(src);
+	if (start + len > src_len)
+		len = src_len - start;
+	str = malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && src[start + i] != '\0')
+	{
+		str[i] = src[start + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
