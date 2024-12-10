f65595c6d86bae01b1ec578c9077a01ee321533f
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-10 12:46:15 +0100

    tools ft_strchr and ft_strjoin created

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,23 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tools.c                                            :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:12:47 by rita              #+#    #+#             */
-/*   Updated: 2024/12/09 17:22:56 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/10 12:45:19 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // TODO strlen		DONE
-// TODO strchr
+// TODO strchr		DONE
 // TODO strjoin
 // TODO substr
 
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include "get_next_line_bonus.h"
 
 // Will count the length of a string
 int	ft_strlen(char *str)
@@ -31,7 +29,9 @@ int	ft_strlen(char *str)
 		count++;
 	return (count);
 }
-
+// if target found will output the string after the target onwards
+// ex : "Hello", 'e' => "ello"
+// ex : "Hello", 'z' => NULL
 char	*ft_strchr(char *src, char target)
 {
 	size_t	i;
@@ -45,4 +45,37 @@ char	*ft_strchr(char *src, char target)
 	}
 	if (src[i] == target)
 		return (&src[i]);
+	return (NULL);
 }
+
+
+// Will join 2 strings.
+char	*ft_strjoin(char const *src1, char const *src2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!src1)
+		src1 = "";
+	if (!src2)
+		src2 = "";
+	str = malloc((ft_strlen(src1) + ft_strlen(src2) + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (src1[i] != '\0')
+	{
+		str[i] != src1[i];
+		i++;
+	}
+	while (src2[i] != '\0')
+	{
+		str[i + j] = src2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (str);		
+}
+
