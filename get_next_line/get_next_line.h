9635b412338b5443c56be276f74238d8e439bad9
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-11 15:09:27 +0100

    get_next_line.h    fixbug

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:57:04 by rita              #+#    #+#             */
-/*   Updated: 2024/12/10 17:35:32 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/11 15:08:03 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,22 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
+# include <fcntl.h> //! to erase (just for testing mode)
+# include <stdio.h> //! to erase (just for testing mode)
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
 
+int		ft_strlen(char *str);
 char	*get_next_line(int fd);
+//void get_next_line(int fd);
+
 char	*ft_strjoin(char *src1, char *src2);
-char	*ft_substr(char const *src, int start, int len);
+char	*ft_substr(char *src, int start, int len);
 // char	*ft_strchr(char *src, char target);
-char	*check_src(char *src, char target);
+
+// will output 1/0 if target is found or not
+int check_src(char *src, char target);
 
 #endif
