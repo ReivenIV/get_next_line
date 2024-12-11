ee5a7b2df7f675e4b0a953f8aec4ff9e286fe5f6
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-11 15:09:42 +0100

    work in progres

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:57:01 by rita              #+#    #+#             */
-/*   Updated: 2024/12/10 17:40:35 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/11 15:07:35 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,49 @@ char	*get_raw_line(int fd, char *remainder)
 	int		res_open;
 
 	//! is these condition necesary ?? 
-	if (remainder && check_src(remainder, '\n') == 1);
+	if (remainder && check_src(remainder, '\n'))
 		return (remainder);
 	buffer_block = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer_block)
 		return (NULL);
 	res_open = 1;
-	while (check_src(remainder, '\n') == 1 && res_open != 0)
+	while (!check_src(remainder, '\n') && res_open != 0)
 	{
 		res_open = read(fd, buffer_block, BUFFER_SIZE);
 		if (res_open == -1)
 			return (free(remainder), remainder = NULL, free(buffer_block), NULL);
-		if (res_open)
+		if (res_open == 0 && !remainder)
+			return (free(buffer_block), NULL);
+		buffer_block[res_open] = 0; // TODO what these line ?
+		remainder = ft_strjoin(remainder, buffer_block);
+		if (!remainder)
+			return (free(buffer_block), NULL);
 	}
+	free(buffer_block);
+	return (remainder);
 }
+
+//! Just for testing 
+static char	*ft_strdup(char *str)
+{
+	int		i;
+	int		str_len;
+	char	*dup;
+
+	i = 0;
+	str_len = ft_strlen(str);
+	dup = (char *)malloc(str_len * sizeof(char) + 1);
+	if (dup == NULL)
+		return (NULL);
+	while (i < str_len)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
+
 
 char	*get_next_line(int fd)
 {
@@ -44,9 +73,23 @@ char	*get_next_line(int fd)
 	raw_line = get_raw_line(fd, remainder);
 	if (!raw_line)
 		return (NULL);
+	printf("%s", raw_line);
 	// next_line = clean_raw_line(raw_line);
 	// remainder = update_remainder(raw_line);
 	// return (next_line);
-	char test[] = "lorem";
+	char *test = ft_strdup("lorem");
+
 	return (test);
+}
+int	main(void)
+{
+	char	*raw_line;
+	int		i = 1;
+	int		fd;
+
+	fd = open("test.txt", O_RDONLY);
+	
+	raw_line = get_next_line(fd);
+	free(raw_line);
+	return (0);
 }
