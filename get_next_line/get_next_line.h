6cc1e0e634d2347f9d077960f7e42cbfdae51ace
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-11 16:44:04 +0100

    update headers

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:57:04 by rita              #+#    #+#             */
-/*   Updated: 2024/12/11 15:08:03 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/11 16:07:05 by rita             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,12 @@
 # include <stdio.h> //! to erase (just for testing mode)
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 5
 # endif
 
 int		ft_strlen(char *str);
+char	*ft_strdup(char *str);
+
 char	*get_next_line(int fd);
 //void get_next_line(int fd);
 
