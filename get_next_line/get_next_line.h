3a87d799afcb00e57266194eb5f6a4b72271265f
ReivenIV <fwebe-ir@student.42perpignan.fr>
2024-12-17 12:48:33 +0100

    headers cleaned

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rita <rita@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: fwebe-ir <fwebe-ir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 11:57:04 by rita              #+#    #+#             */
-/*   Updated: 2024/12/11 16:49:38 by rita             ###   ########.fr       */
+/*   Updated: 2024/12/17 12:47:27 by fwebe-ir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-// if Bsize is bigger than 1M 
+// if Bsize is bigger than 1M we block it at 1M
 # if BUFFER_SIZE > 1000000
 #	undef BUFFER_SIZE
 #	define BUFFER_SIZE 1000000
@@ -32,15 +32,9 @@
 
 int		ft_strlen(char *str);
 char	*ft_strdup(char *str);
-
 char	*get_next_line(int fd);
-//void get_next_line(int fd);
-
 char	*ft_strjoin(char *src1, char *src2);
 char	*ft_substr(char *src, int start, int len);
-// char	*ft_strchr(char *src, char target);
-
-// will output 1/0 if target is found or not
 int check_src(char *src, char target);
 
 #endif
